d642ca40a826eaa4e0c137071797719861ae900a
zeus0z <amorimzeus@gmail.com>
2025-12-24 15:19:59 -0300

    fix: fixed global pipes being on the wrong line on main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
