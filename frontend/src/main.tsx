973596942021b09fc129d2d23bafb48d1ca6e190
zeus0z <amorimzeus@gmail.com>
2025-12-26 16:29:47 -0300

    chore: updated readme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider value={system}>
-      <App />
+    <App />
     </ChakraProvider>
   </StrictMode>,
 )
